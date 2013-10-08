c30a10de27c2293dcbbaef906cf4c074a2ba5895
Brian Leighton <brian@bigroomstudios.com>
2013-10-08 18:18:12 -0400

    58340090 | refactoring

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -35,7 +35,7 @@ class RegistrationController extends Controller
 	/**
 	 * 
 	 */
-	public function registerAction(Request $request) {
+	public function ajaxRegisterAction(Request $request) {
 		
 		//initialize some shit!
 		$formFactory = $this->container->get('fos_user.registration.form.factory');
@@ -130,5 +130,104 @@ class RegistrationController extends Controller
 		return $response;
 		
 	}
+	
+	/**
+	 * 
+	 
+	public function registerAction(Request $request) {
+		
+		//initialize some shit!
+		$formFactory = $this->container->get('fos_user.registration.form.factory');
+		$userManager = $this->container->get('fos_user.user_manager');
+		$user = $userManager->createUser();
+		$user->setEnabled(true);
+		$form = $formFactory->createForm();
+		$form->setData($user);
+		
+		//if the form method is post
+		if ('POST' === $request->getMethod()) {
+			
+			//bind the request
+		    $form->bind($request);
+			
+			//if the form is valid
+		    if($form->isValid()) {
+		    	
+				//save the user
+				$userManager->updateUser($user);
+				
+				//log the new user in
+				$token = new UsernamePasswordToken($user, $user->getPassword(), "public", $user->getRoles());
+				$this->container->get("security.context")->setToken($token);
+				
+				// Trigger login event
+				$event = new InteractiveLoginEvent($request, $token);
+				$this->container->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+				
+				//get the form data
+				$form_data = $form->getData();
+				$first_name = $form_data->getFirstName();
+				
+				//send the registration email
+				$message = \Swift_Message::newInstance()
+					->setSubject("Welcome to Hazel, $first_name")
+					->setFrom('no-reply@bigroomstudios.com')
+					->setTo($form_data->getEmail())
+					->setBody("Example Text");
+				
+				$this->get('mailer')->send($message);
+				
+				//$route = $this->get('router')->generate('property_claim');
+				
+				//redirect the user to the dashboard
+				$response = array(
+					'success' => true,
+					'redirect' => $route,
+				);
+				
+				$response = new Response(json_encode($response));
+				$response->headers->set('Content-Type', 'application/json');
+				
+				return $response;
+				
+		    }
+			else {
+				
+				$errors = $form->getErrors();
+				
+				$error_template = $errors[0]->getMessageTemplate();
+				
+				if($error_template == 'fos_user.username.already_used') {
+					$message = 'User already exists';
+				}
+				else {
+					$message = 'Unknown error occurred';
+				}
+				
+				//generate an error message
+				$response = array(
+					'success' => false,
+					'message' => $message,
+				);
+				$response = new Response(json_encode($response));
+				$response->headers->set('Content-Type', 'application/json');
+				return $response;
+				
+			}
+			
+		}
+		
+		//something else should happen here.  They are supposed to access this via a post request...I think a refactoring of this controller will take care of it...so leaving it for now
+		$response = array(
+			'success' => false,
+			'message' => 'Incorrect method',
+		);
+		
+		$response = new Response(json_encode($response));
+		$response->headers->set('Content-Type', 'application/json');
+		
+		return $response;
+		
+	}*/
 	
 }
